24d91c53c69f52ba52d7cd931d6570e07ef0e483
JamestheDon <jamesdonhawkins@gmail.com>
2021-11-28 08:28:25 -0800

    cleaning up white space.

--- controllers/getBitVer.js
+++ controllers/getBitVer.js
@@ -1,12 +1,5 @@
-"use strict";
-const cp = require("child_process");
-const fs = require("fs");
-
-const reset = "\x1b[0m";
-const cyan = "\x1b[36m";
-
 /**
- *  @desc this module uses sys cmds `curl` & `grep` to retreive \n
+ *  @SPEC this module uses sys cmds `curl` & `grep` to retreive \n
  *        bitcoin core binary releases from bitcoincore.org and Write found versions to database.
  *
  *        Compare versions to btc-core thats installed to found versions.
@@ -18,46 +11,43 @@ const cyan = "\x1b[36m";
  * https://bitcoin.org/bin/
  *
  */
+"use strict";
+const cp = require("child_process");
+const fs = require("fs");
+const reset = "\x1b[0m";
+const cyan = "\x1b[36m";
 
-const getBitVer = () => {
-  const regeex = ".......-....-d.d.d";
+const getBitVer = () => { 
+  // const regeex = ".......-....-d.d.d";
   //.......\-....\-\(\d\+\.\)\?\(\d+\.\)\?\(\*|\d\+\)$
-  try {
+  
     // curl Bitcoin COre versions avail for download.
-    const curl = cp.spawn("curl", ["https://bitcoincore.org/bin/"], {
-      encoding: "utf-8",
-    });
-    // `grep` for keyword 'bitcoin-core'
+    // --silent option stops curl.stderr from receiving the curl progess data.
+    const curl = cp.spawn(
+      "curl",
+      ["--silent", "https://bitcoincore.org/bin/"],
+      {
+        encoding: "utf-8",
+      }
+    );
     const grep = cp.spawn("grep", ["bitcoin-core"], { encoding: "utf-8" });
     //  Piping of `curl` to `grep`
     curl.stdout.pipe(grep.stdin);
-    //  outputs to terminal
-    //  grep.stdout.pipe(process.stdin);
-
     // curl ERRORS
     curl.stderr.on("data", (data) => {
-      /** @BUG Why  */
-      console.log(
-        "STDERRRR1:=>",
-        data.toString(),
-        "Is the `curl` cmd installed?"
-      );
+      /** @BUG Why is this logging curl progress data? */
+      console.log(data.toString());
     });
-
     // grep ERRORS
     grep.stderr.on("data", (data) => {
       console.log("STDERRRR2:=>", data, "\n GREP ERROR");
     });
-
     let buffer = "";
-
     grep.stdout.on("data", (data) => {
-      /** @TODO take care of empty strings (edge case). */ 
       buffer += data;
       const isoString = buffer.split("\n");
       const final = [];
-
-      isoString.map((item, i) => {
+      isoString.map((item) => {
         const version = {
           ver: "",
         };
@@ -66,27 +56,26 @@ const getBitVer = () => {
           return final.push(version);
         }
         if (item.slice(28, 29) === '"') {
-          version.ver = item.slice(9, 28) ;
+          version.ver = item.slice(9, 28);
           return final.push(version);
         }
         if (item.slice(28, 29) === ">") {
-          version.ver = item.slice(9, 27) ;
+          version.ver = item.slice(9, 27);
           return final.push(version);
         }
-        if (item.slice(9, 26)  === "") {
-          return 
+        if (item.slice(9, 26) === "") {
+          return;
         }
         version.ver = item.slice(9, 29);
         final.push(version);
       });
-
-
 
       for (let i = final.length; i--; ) {
         console.log("Bitcoin Core Versions:", final[i]);
       }
 
       console.log(`${cyan}FINALLY${reset}: ${final.length}`);
+
       fs.writeFileSync(
         "./versions.json",
         JSON.stringify(final),
@@ -99,13 +88,11 @@ const getBitVer = () => {
       );
     });
 
-    grep.stdout.on("close", () => {
-      console.log("grep finished...");
-      process.exit(0);
+    grep.stdout.on("close", (data) => {
+      console.log("grep finished...", data);
+      // process.exit(0);
     });
-  } catch (err) {
-    console.log("ERROR", err);
-  }
+
 };
 
 /**
